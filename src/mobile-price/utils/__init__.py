93399d1f7fcf4398189f146d5e5353495c307327
SunilKumar-ugra <ugargolsunilkumar@gmail.com>
2024-06-10 12:54:18 +0530

    Utility

--- src/mobile-price/utils/__init__.py
+++ src/mobile-price/utils/__init__.py
@@ -1,7 +1,7 @@
 import os
 from box.exceptions import BoxValueError
 import yaml
-from mice import logger
+from mobile-price import logger
 import json
 import joblib
 from ensure import ensure_annotations
