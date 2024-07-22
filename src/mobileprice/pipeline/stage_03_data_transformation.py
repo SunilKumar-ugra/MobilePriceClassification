ca5eba3c7a73e98ae50ecd9c5123860277a4ca4f
SunilKumar-ugra <ugargolsunilkumar@gmail.com>
2024-07-22 22:16:33 +0530

    PipeLine-Transform

---
+++ src/mobileprice/pipeline/stage_03_data_transformation.py
@@ -0,0 +1,25 @@
+from mobileprice import logger
+from mobileprice.config import ConfigurationManager
+from mobileprice.components.data_transformation import DataTransformation
+
+
+class DataTransformationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_transformation_config = config.get_data_transformation_config()
+        data_transformation = DataTransformation(config=data_transformation_config)
+        data_transformation.transformation()
+
+if __name__ == '__main__':
+    STAGE_NAME = "Data Transformation stage"
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+        data_transformation = DataTransformationTrainingPipeline()
+        data_transformation.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+            logger.exception(e)
+            raise e
