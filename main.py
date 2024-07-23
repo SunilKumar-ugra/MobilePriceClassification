da8a7970c8057502e2f743682f47c0fa91268e32
SunilKumar-ugra <ugargolsunilkumar@gmail.com>
2024-07-23 14:39:42 +0530

    Main-Train

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from mobileprice import logger
+
+
 from mobileprice.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-
-
 STAGE_NAME = "Data Ingestion Stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
@@ -12,8 +12,9 @@ except Exception as e:
         logger.exception(e)
         raise e
 
-from mobileprice.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
+
+from mobileprice.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 STAGE_NAME = "Data Validation stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
@@ -28,11 +29,23 @@ except Exception as e:
 
 from mobileprice.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 STAGE_NAME = "Data Transformation stage"
-
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_transformation = DataTransformationTrainingPipeline()
    data_transformation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+from mobileprice.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+STAGE_NAME = "Model Trainer stage"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainerTrainingPipeline()
+   model_trainer.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
